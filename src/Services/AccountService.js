ce7f89c245e187ab91275fe1596fae21febce017
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-06-06 04:09:06 -0400

    Completed AccountService

--- src/Services/AccountService.js
+++ src/Services/AccountService.js
@@ -3,10 +3,9 @@ import APIService from './APIService'
 import Cookies from 'js-cookie';
 
 class AccountService extends APIService {
-
     IsUserLoggedIn() {
         var user = this.GetLoggedInUser();
-        if (user == undefined) {
+        if (user === undefined) {
             return false;
         }
         return true;
@@ -22,22 +21,97 @@ class AccountService extends APIService {
         //alert(id + "|" + authToken + "|" + apiUrl);
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
-            if (this.status == 200) {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
                 var user = JSON.parse(this.responseText);
                 //alert("Data Loaded: " + user);
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(user);
                 }    
             }
             else {
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(this.responseText);
                 }
             }
         }; // end of state change: it can be after some time (async)
-        xhr.onerror
         xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send();
+    };
+
+    CreateUserUpdateData(currentPassword, newUsername, newEmail, newPassword) {
+        return ({
+            currentPassword: currentPassword,
+            newUsername: newUsername,
+            newEmail: newEmail,
+            newPassword: newPassword
+        });
+    }
+
+    PreformUpdateAccount(id, authToken, userUpdateData, onComplete) {
+        var apiUrl = this.APIBaseUrl + "/Account/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var user = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + user);
+                if (onComplete !== null) {
+                    onComplete(user);
+                }    
+            }
+            else {
+                if (onComplete !== null) {
+                    onComplete(this.responseText);
+                }
+            }
+        }; // end of state change: it can be after some time (async)
+        xhr.open("PUT", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({
+            currentPassword: userUpdateData.currentPassword,
+            newUsername: userUpdateData.newUsername,
+            newEmail: userUpdateData.newEmail,
+            newPassword: userUpdateData.newPassword
+        }));
+    };
+
+    PreformDeleteAccount(id, authToken, onComplete) {
+        var apiUrl = this.APIBaseUrl + "/Account/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                if (onComplete !== null) {
+                    onComplete(this.responseText);
+                } 
+            }
+        }; // end of state change: it can be after some time (async)
+        xhr.open("DELETE", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send();
+    };
+
+    PreformCheckAuthentication(id, authToken, onComplete) {
+        var apiUrl = this.APIBaseUrl + "/Authentication/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (onComplete !== null) {
+                onComplete(this.responseText);
+            } 
+        }; // end of state change: it can be after some time (async)
+        xhr.open("POST", apiUrl, true);
         xhr.setRequestHeader('Accept', 'application/json');
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.setRequestHeader('Authorization', authToken);
@@ -49,21 +123,20 @@ class AccountService extends APIService {
         //alert(username + "|" + password + "|" + apiUrl);
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
-            if (this.status == 200) {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
                 var authenticatedUser = JSON.parse(this.responseText);
                 //alert("Data Loaded: " + authenticatedUser);
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(authenticatedUser);
                 }    
             }
             else {
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(this.responseText);
                 }
             }
         }; // end of state change: it can be after some time (async)
-        xhr.onerror
         xhr.open("POST", apiUrl, true);
         xhr.setRequestHeader('Accept', 'application/json');
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -94,13 +167,13 @@ class AccountService extends APIService {
             success: function (response) {
                 var authenticatedUser = JSON.parse(response);
                 //alert("Data Loaded: " + authenticatedUser);
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(authenticatedUser);
                 }                
             },
             error: function (xhr, status, error) {
                 //alert(xhr.responseText);
-                if (onComplete != null) {
+                if (onComplete !== null) {
                     onComplete(xhr.responseText);
                 }
             }
