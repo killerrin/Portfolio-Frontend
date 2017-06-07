6f16f83665f82ed4b3c4308ee334981b7116106a
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-06-06 23:29:46 -0400

    Added a TagService

---
+++ src/Services/TagService.js
@@ -0,0 +1,277 @@
+import $ from 'jquery'
+import APIService from './APIService'
+
+class TagService extends APIService {
+    constructor(onComplete, onFailed) {
+        super(onComplete, onFailed);
+    }
+
+    PreformGetAllTags() {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/Tag";
+        //alert(apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformGetTag(id) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/Tag/" + id;
+        //alert(id + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformCreateTag(name, tagTypeID, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/Tag";
+        //alert(name + "|" + tagTypeID + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("POST", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({
+            name: name,
+            tagTypeID: tagTypeID
+        }));
+    };
+    PreformUpdateTag(id, name, tagTypeID, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/Tag/" + id;
+        //alert(id + "|" + name + "|" + tagTypeID + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("PUT", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({
+            id: id,
+            name: name,
+            tagTypeID: tagTypeID
+        }));
+    };
+    PreformDeleteTag(id, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/Tag/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("DELETE", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send();
+    };
+
+    PreformGetAllTagTypes() {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/TagType";
+        //alert(apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformGetTagType(id) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/TagType/" + id;
+        //alert(id + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformCreateTagType(name, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/TagType";
+        //alert(name + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("POST", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({name:name}));
+    };
+    PreformUpdateTagType(id, name, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/TagType/" + id;
+        //alert(id + "|" + name + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("PUT", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({
+            id: id,
+            name:name
+        }));
+    };
+    PreformDeleteTagType(id, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/TagType/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("DELETE", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send();
+    };
+};
+export default TagService;
