17fd7dcaf6b2325a1c751bd6603d5cc1e6d3dbd9
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-06-07 03:27:32 -0400

    Added RelatedItemService

---
+++ src/Services/RelatedItemService.js
@@ -0,0 +1,142 @@
+import $ from 'jquery'
+import APIService from './APIService'
+
+class RelatedItemService extends APIService {
+    constructor(onComplete, onFailed) {
+        super(onComplete, onFailed);
+    }
+
+    PreformGetAllRelatedItems() {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/RelatedItem";
+        //alert(apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformGetRelatedItem(id) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/RelatedItem/" + id;
+        //alert(id + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("GET", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+    };
+    PreformCreateRelatedItem(name, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/RelatedItem";
+        //alert(name + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("POST", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({name:name}));
+    };
+    PreformUpdateRelatedItem(id, name, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/RelatedItem/" + id;
+        //alert(id + "|" + name + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("PUT", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send(JSON.stringify({
+            id: id,
+            name:name
+        }));
+    };
+    PreformDeleteRelatedItem(id, authToken) {
+        var self = this;
+        var apiUrl = this.APIBaseUrl + "/RelatedItem/" + id;
+        //alert(id + "|" + authToken + "|" + apiUrl);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                var parsedResponse = JSON.parse(this.responseText);
+                //alert("Data Loaded: " + parsedResponse);
+                if (self.onComplete !== null) {
+                    self.onComplete(parsedResponse);
+                }    
+            }
+            else {
+                if (self.onFailed !== null) {
+                    self.onFailed(this.responseText);
+                }
+            }
+        };
+        xhr.open("DELETE", apiUrl, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', authToken);
+        xhr.send();
+    };
+};
+
+export default RelatedItemService;
